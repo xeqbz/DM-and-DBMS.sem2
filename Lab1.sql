163cb60f514f2829afe92f4881ddaff4c1fa813d
xeqbz <tzaruk.vadick@yandex.ru>
2025-01-27 16:37:08 +0300

    Lab 1 done

--- Lab1.sql
+++ Lab1.sql
@@ -53,3 +53,108 @@ END;
 BEGIN
     DBMS_OUTPUT.PUT_LINE(CREATEINSERTFUNCTION(5));
 END;
+
+--- Task 5 ---
+-- INSERT --
+CREATE OR REPLACE PROCEDURE InsertIntoMyTable(
+    p_id IN MyTable.id%TYPE,
+    p_val IN MyTable.val%TYPE
+) IS
+BEGIN
+    INSERT INTO MyTable(id, val)
+    VALUES (p_id, p_val);
+    COMMIT;
+    DBMS_OUTPUT.PUT_LINE('Строка успешно добавлена: ID = ' || p_id || ', val = ' || p_val);
+END;
+/
+
+BEGIN
+   InsertIntoMyTable(1, 100);
+   InsertIntoMyTable(2,200);
+END;
+/
+
+-- UPDATE --
+CREATE OR REPLACE PROCEDURE UpdateMyTable(
+    p_id IN MyTable.id%TYPE,
+    p_val in MyTable.val%TYPE
+) IS
+BEGIN
+    UPDATE MyTable
+    SET val = p_val
+    WHERE id = p_id;
+
+    IF SQL%ROWCOUNT = 0 THEN
+        DBMS_OUTPUT.PUT_LINE('Ошибка: строка с ID = ' || p_id || ' не найдена.');
+    ELSE
+        COMMIT;
+        DBMS_OUTPUT.PUT_LINE('Строка успешно обновлена: ID = ' || p_id || ', новое val = ' || p_val);
+    END IF;
+EXCEPTION
+    WHEN OTHERS THEN
+        DBMS_OUTPUT.PUT_LINE('Ошибка: ' || SQLERRM);
+END;
+/
+
+BEGIN
+    UpdateMyTable(1, 150);
+END;
+/
+
+-- DELETE --
+CREATE OR REPLACE PROCEDURE DeleteFromMyTable(
+    p_id IN MyTable.id%TYPE
+) IS
+BEGIN
+    DELETE FROM MyTable
+    WHERE id = p_id;
+
+    IF SQL%ROWCOUNT = 0 THEN
+        DBMS_OUTPUT.PUT_LINE('Ошибка: строка с ID = ' || p_id || ' не найдена.');
+    ELSE
+        COMMIT;
+        DBMS_OUTPUT.PUT_LINE('Строка успешно удалена: ID = ' || p_id);
+    END IF;
+EXCEPTION
+    WHEN OTHERS THEN
+        DBMS_OUTPUT.PUT_LINE('Ошибка: ' || SQLERRM);
+END;
+/
+
+BEGIN
+    DeleteFromMyTable(10000);
+END;
+/
+
+--- Task 6 ---
+CREATE OR REPLACE FUNCTION CalculateReward(
+    f_monthly_salary IN NUMBER,
+    f_bonus_percent IN NUMBER
+) RETURN NUMBER IS
+    r_reward NUMBER;
+BEGIN
+    IF f_monthly_salary IS NULL OR f_monthly_salary < 0 THEN
+        RAISE_APPLICATION_ERROR(-20001, 'Некорректное значение месячной зарплаты.');
+    END IF;
+
+    IF f_bonus_percent IS NULL OR f_bonus_percent < 0 THEN
+        RAISE_APPLICATION_ERROR(-20002, 'Некорректное значение процента премиальных.');
+    END IF;
+
+    r_reward := (1 + f_bonus_percent / 100) * 12 * f_monthly_salary;
+
+    RETURN r_reward;
+EXCEPTION
+    WHEN OTHERS THEN
+        DBMS_OUTPUT.PUT_LINE('Ошибка: ' || SQLERRM);
+        RETURN NULL;
+END;
+/
+
+DECLARE
+    r_reward NUMBER;
+BEGIN
+   r_reward := CalculateReward(50000, 10);
+   DBMS_OUTPUT.PUT_LINE('Общее вознаграждение за год: ' || r_reward);
+END;
+/
